14595cd9b768ff987822faae0c0c7ff14f6bc90b
Guilherme Arnhold <Guilherme.arnhold@megaembalagens.com.br>
2020-12-02 13:18:23 -0300

    course folder

---
+++ JS/js-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+let country = "Brazil";
+let continent = "South America";
+let population = 209;
+let isIsland = false;
+let language;
+
+/*console.log(country);
+console.log(continent);
+console.log(population);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+
+language = "Portuguese";
+console.log(language);
+//const RG = 032132143132;
+//const RG = 3213213123211312;
+
+const thisYear = 2020;
+const ageJoao = thisYear - 1990;
+const ageMaria = thisYear - 1995;
+
+console.log(ageJoao, ageMaria);
