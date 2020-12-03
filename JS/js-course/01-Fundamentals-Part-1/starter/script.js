80602752d993667bdf4c2116c7e180b4296eefe7
Guilherme Arnhold <Guilherme.arnhold@megaembalagens.com.br>
2020-12-03 13:21:36 -0300

    Estudo

--- JS/js-course/01-Fundamentals-Part-1/starter/script.js
+++ JS/js-course/01-Fundamentals-Part-1/starter/script.js
@@ -1,25 +1,99 @@
+
+
 let country = "Brazil";
 let continent = "South America";
 let population = 209;
 let isIsland = false;
 let language;
 
-/*console.log(country);
+/*
+console.log(country);
 console.log(continent);
 console.log(population);
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
-*/
 
-language = "Portuguese";
+
+language = "Português";
 console.log(language);
-//const RG = 032132143132;
-//const RG = 3213213123211312;
+const RG = 032132143132;
+const RG = 3213213123211312;
 
 const thisYear = 2020;
 const ageJoao = thisYear - 1990;
 const ageMaria = thisYear - 1995;
-
 console.log(ageJoao, ageMaria);
+
+
+let metadeDaPopulação;
+let outraMetade;
+
+metadeDaPopulação = population / 2;
+outraMetade = population / 2;
+console.log(population);
+console.log("Metade da população: " + metadeDaPopulação, "Outra metade: " + outraMetade);
+population++;
+console.log("Metade da população: " + metadeDaPopulação, "Outra metade: " + outraMetade);
+if (population > 6) {
+    console.log("Meu país possui uma população maior do que 6 milhões, temos atualmente: " + population);
+}
+else {
+    console.log("Meu país possui uma população menor do que 6 milhões, temos atualmente: " + population);
+}
+
+if (population > 33) {
+    console.log("Meu país possui uma população maior do que 33 milhões, temos atualmente: " + population);
+}
+else {
+    console.log("Meu país possui uma população menor do que 33 milhões, temos atualmente: " + population);
+}
+
+
+
+let description = "O brasil está na América do Sul e seus 209 milhões de pessoas falam português.";
+console.log(description);
+
+
+let description = `o ${country} está na ${continent} e seus ${population} milhões de pessoas falam ${language}`;
+console.log(description);
+
+let diferenca = 121 - population;
+
+
+if (population > 33) {
+    console.log(`a população de ${country} está acima da média.`);
+}
+else {
+    console.log(`a população de ${country} está  ${diferenca} milhões abaixo da média`)
+}
+
+
+let massMarks = 95;
+let heightMarks = 1.88;
+let massJohn = 85;
+let heightJohn = 1.76;
+
+
+let bmiMarks = massMarks / (heightMarks ** 2);
+let bmiJohn = massJohn / (heightJohn ** 2);
+
+if (bmiMarks > bmiJohn) {
+    console.log(`O BMI de Marks(${bmiMarks}) é superior ao de John(${bmiJohn})`);
+}
+else {
+    console.log(`O BMI de John (${bmiJohn}) é superior ao de Marks(${bmiMarks})`);
+}
+*/
+
+
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+
+
+
