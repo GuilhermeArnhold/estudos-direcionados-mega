13775e942706bd870a553e87fb13ddde9f7892f6
Guilherme Arnhold <Guilherme.arnhold@megaembalagens.com.br>
2020-12-08 13:09:39 -0300

    Estudos.

--- JS/js-course/02-Fundamentals-Part-2/starter/script.js
+++ JS/js-course/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,63 @@
-'use strict'; 
+'use strict';
+
+/*
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} milion people and your capital city is ${capitalCity}`;
+
+}
+
+
+let countryOne = describeCountry("Brazil", 209, "Brasilia");
+let countryTwo = describeCountry("China", 328, "Washington");
+let countryThree = describeCountry("Russia", 144, "Moscou");
+console.log(countryOne);
+console.log(countryTwo);
+console.log(countryThree);
+
+
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+};
+
+const percentualNoruega = percentageOfWorld1(5);
+const percentualAlemanha = percentageOfWorld1(83);
+const percentualPaquistao = percentageOfWorld1(212);
+console.log(percentualNoruega, percentualAlemanha, percentualPaquistao);
+
+
+let populations = [209, 44, 50, 65];
+console.log(populations.length);
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+
+
+if (populations.length == 4) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]), percentageOfWorld1(populations[2]), percentageOfWorld1(populations[3])];
+console.log(percentages);
+
+
+
+const percentageOfWorld3 = population => {
+    return (population / 7900) * 100;
+}
+
+console.log(percentageOfWorld3(50));
+*/
+
+
+
+
