16128b8407bb9febf3c9348c978434bd08c2f49f
Guilherme Arnhold <Guilherme.arnhold@megaembalagens.com.br>
2021-02-01 13:25:00 -0300

    Estudos

--- JS/js-course/14-OOP/starter/script.js
+++ JS/js-course/14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/hr`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/hr`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+mercedes.brake();
+mercedes.brake();
+mercedes.accelerate();
+mercedes.accelerate();
