c6901c9829b433be4707aec0ac75545ee5b26881
Guilherme Arnhold <Guilherme.arnhold@megaembalagens.com.br>
2021-02-03 13:29:23 -0300

    Estudos

--- JS/js-course/14-OOP/starter/script.js
+++ JS/js-course/14-OOP/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
@@ -24,3 +24,68 @@ mercedes.brake();
 mercedes.brake();
 mercedes.accelerate();
 mercedes.accelerate();
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.latest);
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  get speedUS() {
+    return `${this.speed / 1.6} mi/h`;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new Car('Ford', 120);
+console.log(ford.speedUS);
+ford.speedUS = 100;
+console.log(ford.speedUS);
+*/
+
+const Car = function (make, speed) {
+  this.speed = speed;
+  this.make = make;
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h and your charge is ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.accelerate();
